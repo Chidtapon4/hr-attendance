f52c330d85dcae797937034a2027c16a0d2d0ea5
Chidtapon4 <chidpon.tapa@gmail.com>
2026-05-13 14:23:04 +0700

    config: update GAS URL for multi-branch deployment

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   LIFF_ID: '2010069446-CMb7v2X7',
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbzX-8CVtV6D7YFsosHZoHaefmTeUnvrEGvoEzxPRoXq-c1KbPYpWtF77fyzgs557zLZsA/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbyJAZtMgzFsN1DXUdW61kU6E7pGKIv54jKlcj8FfB53qMw81QJuQvMthSEJKcNL7y6p/exec',
   GPS_ACCURACY_THRESHOLD: 100,
   IMAGE_MAX_WIDTH: 800,
   IMAGE_QUALITY: 0.7,
