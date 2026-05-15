a73c9ee262937f56eaec4ba8e0811976f61235cd
Chidtapon4 <chidpon.tapa@gmail.com>
2026-05-15 10:58:03 +0700

    update GAS URL

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   LIFF_ID: '2010069446-CMb7v2X7',
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbzUco4kN9WXnqCs0RovIB8YHg05d7V2V-GysczO2ZnXT_-vHZxWp0VwMsB3Bab0nG1G3Q/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbwoFQLfLMsJ-lf7Wo17DXJ8rYLgrBR0xoxVaf3ScE6r7ehj-g8raCgfke327HZaG_yBrg/exec',
   GPS_ACCURACY_THRESHOLD: 300,
   IMAGE_MAX_WIDTH: 800,
   IMAGE_QUALITY: 0.7,
